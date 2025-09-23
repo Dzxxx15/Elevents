0838e7039e28af762239400a8b725f37fa012832
jussimar <jussimar@jussimarleal.com.br>
2025-09-22 22:22:51 -0300

    Elevents

--- src/views/CriarConta.jsx
+++ src/views/CriarConta.jsx
@@ -1,42 +1,51 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../Css/CriarContaModule.css';
+
 
 function CriarConta() {
   return (
     <>
       <section className="card-container">
-        <div className="card">
-          <div className="card-header orange"></div>
-          <div className="icon">
-            <i className="fas fa-user"></i>
-          </div>
-          <h2>Organizador</h2>
-          <ul className="card-body">
-            <li>Administre seus Eventos com facilidade</li>
-            <li>Reduza gastos com sistemas</li>
-            <li>Reduza o tempo gasto no setup do evento</li>
-          </ul>
-          <Link to="/organizador" className="btn-orange">
-            Organizador
-          </Link>
-        </div>
+  <div className="card">
+    <div className="card-header orange">
+      <div className="icon">
+        <i className="fas fa-user"></i>
+      </div>
+      <h2>Organizador</h2>
+    </div>
 
-        <div className="card">
-          <div className="card-header blue"></div>
-          <div className="icon">
-            <i className="fas fa-user"></i>
-          </div>
-          <h2>Participante</h2>
-          <ul className="card-body">
-            <li>Administre seus Eventos com facilidade</li>
-            <li>Reduza gastos com sistemas</li>
-            <li>Reduza o tempo gasto no setup do evento</li>
-          </ul>
-          <Link to="/participante" className="btn-blue">
-            Participante
-          </Link>
-        </div>
-      </section>
+    <ul className="card-body">
+      <li>Administre seus Eventos com facilidade</li>
+      <li>Reduza gastos com sistemas</li>
+      <li>Reduza o tempo gasto no setup do evento</li>
+    </ul>
+
+    <Link to="/organizador" className="btn-orange">
+      Organizar
+    </Link>
+  </div>
+
+  <div className="card">
+    <div className="card-header blue">
+      <div className="icon">
+        <i className="fas fa-user"></i>
+      </div>
+      <h2>Participante</h2>
+    </div>
+
+    <ul className="card-body">
+      <li>Encontre os melhores lugares para se divertir em um clique</li>
+      <li>Pagamento Direto no Site</li>
+      <li>QR-code para validar sua entrada</li>
+    </ul>
+
+    <Link to="/participante" className="btn-blue">
+      Participar
+    </Link>
+  </div>
+</section>
+
 
       <footer>
         <p>&copy; Elevents</p>
